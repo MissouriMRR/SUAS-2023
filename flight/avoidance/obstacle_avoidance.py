8643c1da28f62dfdc82803abef710f8ddcbe7af8
RexBerry <59031902+RexBerry@users.noreply.github.com>
2022-09-20 19:50:10 -0500

    Add basic stuff

--- flight/avoidance/obstacle_avoidance.py
+++ flight/avoidance/obstacle_avoidance.py
@@ -1,7 +1,19 @@
+"""
+Contains an obstacle avoidance function
+"""
+
+from typing import Optional
+
 from mavsdk import System
+from mavsdk.telemetry import Position
+
+# TODO: Update type to actual type # pylint: disable=fixme
+Coordinate = dict[str, float]
 
 
-def calculate_avoidance_path(drone: System, obstacle: dict) -> list[dict]:
+async def calculate_avoidance_path(
+    drone: System, obstacle: Coordinate, position: Optional[Position]
+) -> list[Coordinate]:
     """
     Given a drone and an obstacle, calculates a path avoiding the obstacle
 
@@ -11,6 +23,9 @@ def calculate_avoidance_path(drone: System, obstacle: dict) -> list[dict]:
         The drone for which the path will be calculated for
     obstacle : dict
         The obstacle to avoid
+    position : Optional[Position] = None
+        The position of the drone
+        The current position of the drone will be used if not provided
 
     Returns
     -------
@@ -18,4 +33,27 @@ def calculate_avoidance_path(drone: System, obstacle: dict) -> list[dict]:
         The avoidance path, consisting of a list of waypoints
     """
 
+    # Get position of drone
+    drone_position: Position
+    if position is None:
+        async for position in drone.telemetry.position():
+            drone_position = position
+            break
+    else:
+        drone_position = position
+
+    # TODO: Do something useful with these variables # pylint: disable=fixme
+    print(obstacle, drone_position)
+
     raise NotImplementedError
+
+
+def main() -> None:
+    """
+    Will do something in the future
+    """
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    main()
