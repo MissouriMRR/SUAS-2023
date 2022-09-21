66d1205bfdbea7fc416b8b01dcc18b404b01b0b2
Ooglely <alennavdic@gmail.com>
2022-09-20 19:46:53 -0500

    Added docstring and fixed code formatting

--- flight/extract_gps.py
+++ flight/extract_gps.py
@@ -1,45 +1,88 @@
+"""Contains the extract_gps() function for extracting data out of
+the provided waypoint data JSON file for the SUAS competition."""
 from collections import namedtuple
-from typing import List, NamedTuple, TypeAlias
+from typing import NamedTuple
 import json
+from typing_extensions import TypedDict
 
-GPSData: TypeAlias = dict(list(NamedTuple), list(NamedTuple), int, int)
+GPSData = TypedDict(
+    "GPSData",
+    {
+        "waypoints": list(NamedTuple(int, int, int)),
+        "boundary_points": list(NamedTuple(int, int)),
+        "altitude_limits": list(int, int),
+    },
+)
+
 
 def extract_gps(path: str) -> GPSData:
-    
-    Waypoint = namedtuple('Waypoint', ['latitude', 'longitude', 'altitude'])
-    BoundaryPoint = namedtuple('BoundaryPoint', ['latitude', 'longitude'])
-    
-    with open(path) as waypoint_data:
-        dataFile = json.load(waypoint_data)
+    """Returns the waypoints, boundary points, and altitude limits from a waypoint data file.
+
+    Args:
+        path (str): File path to the waypoint data JSON file.
+
+    Returns:
+        GPSData: TypedDict[
+            list(NamedTuple(int, int, int)), list(NamedTuple(int, int)), list(int, int)
+        ]
+            The data in the waypoint data file
+            waypoints : list(NamedTuple(int, int, int))
+                Waypoint : NamedTuple(int, int, int)
+                    latitude : int
+                        The latitude of the waypoint.
+                    longitude : int
+                        The longitude of the waypoint.
+                    altitude : int
+                        The altitude of the waypoint.
+            boundary_points : list(NamedTuple(int, int, int))
+                BoundaryPoint : NamedTuple(int, int, int)
+                    latitude : int
+                        The latitude of the boundary point.
+                    longitude : int
+                        The longitude of the boundary point.
+            altitude_limits : list(int, int)
+                altitude_min : int
+                    The minimum altitude that the drone must fly at all times.
+                altitude_max : int
+                    The maximum altitude that the drone must fly at all times.
+    """
+
+    Waypoint = namedtuple("Waypoint", ["latitude", "longitude", "altitude"])
+    BoundaryPoint = namedtuple("BoundaryPoint", ["latitude", "longitude"])
+
+    with open(path, encoding="UTF-8") as data_file:
+        json_data = json.load(data_file)
 
     waypoints = []
-    boundaryPoints = []
+    boundary_points = []
 
-    for i in dataFile['waypoints']:
-        latitude = i['latitude']
-        longitude = i['longitude']
-        altitude = i['altitude']
+    for waypoint in json_data["waypoints"]:
+        latitude = waypoint["latitude"]
+        longitude = waypoint["longitude"]
+        altitude = waypoint["altitude"]
 
         waypoint = Waypoint(latitude, longitude, altitude)
         waypoints.append(waypoint)
-    
-    print('Boundary Points Loaded:')
-    for i in dataFile['flyzones']['boundaryPoints']:
-        latitude = i['latitude']
-        longitude = i['longitude']
 
-        boundaryPoint = BoundaryPoint(latitude, longitude)
-        boundaryPoints.append(boundaryPoint)
+    print("Boundary Points Loaded:")
+    for boundary_point in json_data["flyzones"]["boundaryPoints"]:
+        latitude = boundary_point["latitude"]
+        longitude = boundary_point["longitude"]
 
-    altitudeMin = dataFile['flyzones']['altitudeMin']
-    altitudeMax = dataFile['flyzones']['altitudeMax']
+        boundary_point = BoundaryPoint(latitude, longitude)
+        boundary_points.append(boundary_point)
 
-    return {'waypoints': waypoints, 'boundaryPoints': boundaryPoints, 'altitudeMin': altitudeMin, 'altitudeMax': altitudeMax}
+    altitude_min = json_data["flyzones"]["altitudeMin"]
+    altitude_max = json_data["flyzones"]["altitudeMax"]
+
+    waypoint_data: GPSData = {
+        "waypoints": waypoints,
+        "boundary_points": boundary_points,
+        "altitude_limits": [altitude_min, altitude_max],
+    }
+
+    return waypoint_data
+
 
 if __name__ == "__main__":
-    extract_gps('flight/waypoint_data.json')
-
-    
-
-    
-
+    extract_gps("flight/waypoint_data.json")
