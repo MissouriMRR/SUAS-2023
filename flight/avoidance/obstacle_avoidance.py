80f2efddf7e63b1250967c72f7f2ec42f4236032
RexBerry <59031902+RexBerry@users.noreply.github.com>
2022-09-27 19:30:56 -0500

    Add InputPoint and Point #16

--- flight/avoidance/obstacle_avoidance.py
+++ flight/avoidance/obstacle_avoidance.py
@@ -2,16 +2,26 @@
 Contains a moving obstacle avoidance function
 """
 
+# pylint: disable=fixme
+
 from dataclasses import dataclass
 from typing import Optional
+from typing import Union
 
 from mavsdk import System
-from mavsdk.telemetry import Position
+from mavsdk.telemetry import Position as MavsdkPosition
 
-# TODO: Update type to the correct type # pylint: disable=fixme
-Obstacle = dict[str, float]
+import utm
 
 
+# Input points are dicts with time and UTM coordinate data
+# May change in the future
+# Optional[float] for time, allows type to be used for points without time
+InputPoint = dict[str, Union[float, int, str, Optional[float]]]
+
+
+# TODO: Maybe add a time component
+# TODO: Maybe move to a different file in this directory
 @dataclass
 class Point:
     """
@@ -27,16 +37,65 @@ class Point:
         The UTM zone this point is in
     utm_zone_letter : str
         The letter of the UTM latitude band
+    altitude : float
+        The altitude of the point above sea level, in meters
     """
 
     utm_x: float
     utm_y: float
     utm_zone_number: int
     utm_zone_letter: str
+    altitude: Optional[float]
+
+    @classmethod
+    def from_dict(cls, position_data: InputPoint) -> "Point":
+        """
+        Converts a dict with position data to a Point object
+
+        Parameters
+        ----------
+        position_data : dict[str, Union[float, int, str]]
+            A dict containing at least the following keys:
+            'utm_x', 'utm_y', 'utm_zone_number', 'utm_zone_letter'
+
+        Returns
+        -------
+        A new Point object
+        """
+
+        # TODO: Fix pylint errors
+        return cls(
+            float(position_data["utm_x"]),
+            float(position_data["utm_y"]),
+            int(position_data["utm_zone_number"]),
+            str(position_data["utm_zone_letter"]),
+            None,
+        )
+
+    @classmethod
+    def from_mavsdk_position(cls, position: MavsdkPosition) -> "Point":
+        """
+        Converts a position data dict to a Point object
+
+        Parameters
+        ----------
+        position : mavsdk.telemetry.Position
+            A position from MAVSDK
+
+        Returns
+        -------
+        A new Point object
+        """
+
+        # TODO: Fix pylint errors
+        return cls(*utm.from_latlon(position.latitude_deg, position.longitude_deg), None)
 
 
 async def calculate_avoidance_path(
-    drone: System, obstacle: Obstacle, position: Optional[Position]
+    drone: System,
+    obstacle_data: list[InputPoint],
+    position: Optional[MavsdkPosition],
+    avoidance_radius: float = 10.0,
 ) -> list[Point]:
     """
     Given a drone and a moving obstacle, calculates a path avoiding the obstacle
@@ -45,11 +104,13 @@ async def calculate_avoidance_path(
     ----------
     drone : System
         The drone for which the path will be calculated for
-    obstacle : dict
-        The obstacle to avoid
+    obstacle_data : list[InputPoint]
+        Previously known positions
     position : Optional[Position] = None
         The position of the drone
         The current position of the drone will be used if not provided
+    avoidance_radius : float
+        The radius of the sphere, in meters, from the predicted center of the obstacle that the drone should avoid
 
     Returns
     -------
@@ -58,16 +119,19 @@ async def calculate_avoidance_path(
     """
 
     # Get position of drone
-    drone_position: Position
+    raw_drone_position: MavsdkPosition
     if position is None:
         async for position in drone.telemetry.position():
-            drone_position = position
+            raw_drone_position = position
             break
     else:
-        drone_position = position
+        raw_drone_position = position
 
-    # TODO: Do something useful with these variables # pylint: disable=fixme
-    print(obstacle, drone_position)
+    # Convert drone position to UTM Point
+    drone_position: Point = Point.from_mavsdk_position(raw_drone_position)
+
+    # TODO: Do something useful with these variables
+    print(obstacle_data, drone_position, avoidance_radius)
 
     raise NotImplementedError
 
