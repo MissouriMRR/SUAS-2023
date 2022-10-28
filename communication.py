d24a08a837785cf5867e394bbd337009f892150a
Connor Hallemann <cjhr95@umsystem.edu>
2022-10-27 17:56:18 -0700

    Logging & Comm Object

---
+++ communication.py
@@ -0,0 +1,31 @@
+"""Communication object to pass between flight and vision code"""
+
+
+class Communication:
+    """
+    Object that is passed between flight and vision code to convey
+    current state, and update if necessary
+
+    Attributes
+    ----------
+    __state: str
+        The name of the current state in the Flight State Machine
+
+    Methods
+    -------
+    state() -> str
+        Returns the name of the current state
+    state(new_state) -> None
+        Updates the name of the current state
+    """
+    def __init__(self) -> None:
+        self.__state: str = "Start_State"
+
+    @property
+    def state(self) -> str:
+        return self.__state
+
+    @state.setter
+    def state(self, new_state: str) -> None:
+        self.__state = new_state
+        return
---
+++ logger.py
@@ -0,0 +1,55 @@
+"""Functions and Configurations for logging flight & vision data during flight"""
+
+import logging
+from multiprocessing import Queue
+from logging import Formatter, FileHandler, StreamHandler
+from logging.handlers import QueueHandler, QueueListener
+from colorlog import ColoredFormatter
+from datetime import datetime
+
+LOG_FILE: str = f"logs/{datetime.now()}.log"
+LOG_LEVEL = logging.DEBUG
+LOG_FORMAT: str = "%(levelname)s | %(asctime)s @ %(processname)s:%(funcName)s > %(message)s"
+COLOR_LOG_FORMAT: str = "(log_color)s%(levelname)s | %(asctime)s @  %(processName)s:%(funcName)s > %(message)s%(reset)s"
+
+
+def init_logger(queue: Queue) -> QueueListener:
+    """
+    Initializes a QueueListener object to be used throughout the competition code to contain log messages
+
+    Parameters
+    ----------
+    queue : Queue
+        Data structure to hold logging messages
+
+    Returns
+    -------
+    QueueListener
+        Object to process log messages
+    """
+    file_formatter: Formatter = logging.Formatter(LOG_FORMAT)
+    file: FileHandler = logging.FileHandler(LOG_FILE, "logFile")
+    file.setFormatter(file_formatter)
+
+    console_formatter: Formatter = ColoredFormatter(COLOR_LOG_FORMAT)
+    console: StreamHandler = logging.StreamHandler()
+    console.setFormatter(console_formatter)
+
+    return QueueListener(queue, file, console)
+
+
+def worker_configurer(queue: Queue) -> None:
+    """
+    Configures the logger to send logging messages to QueueListener process
+
+    Parameters
+    ----------
+    queue : Queue
+        Data structure that holds logging messages
+    """
+    queue_handler: QueueHandler = QueueHandler(queue)
+    root = logging.getLogger()
+    root.addHandler(queue_handler)
+    root.setLevel(LOG_LEVEL)
+    return
+
