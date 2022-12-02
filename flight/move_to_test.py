bb0aa1294c19faed3b25583323930b62eae3a940
Dheeraj <dheerajmb19@gmail.com>
2022-12-01 17:00:19 -0800

    corrections for the two files

--- flight/move_to_test.py
+++ flight/move_to_test.py
@@ -3,46 +3,58 @@ Main driver code for moving drone to each waypoint
 """
 
 import asyncio
-import goto
 from mavsdk import System
 import logging
-from typing import List
 import sys
-import extract_gps
+
+import flight.extract_gps
+import flight.Waypoint.goto
 
 
 async def run() -> None:
     """
     This function is a driver to test the goto function and runs through the
     given waypoints in the lats and longs lists at the altitude of 100.
-    Makes the drone move to each location in the lats and longs arrays at the altitude of 100 and
+    Makes the drone move to each location in the lats and longs arrays at the altitude of 100.
+
     Notes
     -----
     Currently has 3 values in each the Lats and Longs array and code is looped and will stay in that loop
     until the drone has reached each of locations specified by the latitude and longitude and
     continues to run until forced disconnect
     """
-    # Put all latitudes, longitudes and altitudes into seperate arrays
-    lats: List[float] = []
-    longs: List[float] = []
-    altitudes: List[float] = []
+    # Defining file path constant for extract_gps
+    PATH: str = "./data/waypoint_data.json"
+    # Defining system address
+    SYSTEM_ADDRESS: str = "udp://:14540"
+    # Defining altitude and speed
+    ALTITUDE: int = 12
+    SPEED: int = 20
+    # Defining fast_param constant
+    FAST_PARAM: float = 0.83
 
-    waypoint_data = extract_gps.extract_gps("./data/waypoint_data.json")
+    # Put all latitudes, longitudes and altitudes into seperate arrays
+    lats: list[float] = []
+    longs: list[float] = []
+    altitudes: list[float] = []
+
+    waypoint_data = extract_gps.extract_gps(PATH)
     waypoints = waypoint_data['waypoints']
-    for i in waypoints:
-        lats.append(i.latitude)
-        longs.append(i.longitude)
-        altitudes.append(i.altitude)
-    # print(lats)
-    # print(longs)
-    # print(altitudes)
+    #waypoint: tuple
+    
+    waypoint: tuple
+    for waypoint in waypoints:
+        lats.append(waypoint.latitude)
+        longs.append(waypoint.longitude)
+        altitudes.append(waypoint.altitude)
+        
     # create a drone object
     drone: System = System()
-    await drone.connect(system_address="udp://:14540")
+    await drone.connect(SYSTEM_ADDRESS)
 
     # initilize drone configurations
-    await drone.action.set_takeoff_altitude(12)
-    await drone.action.set_maximum_speed(20)
+    await drone.action.set_takeoff_altitude(ALTITUDE)
+    await drone.action.set_maximum_speed(SPEED)
 
     # connect to the drone
     logging.info("Waiting for drone to connect...")
@@ -67,8 +79,9 @@ async def run() -> None:
     await asyncio.sleep(10)
 
     # move to each waypoint in mission
+    point: int
     for point in range(len(lats)):
-        await goto.move_to(drone, lats[point], longs[point], 100, True)
+        await goto.move_to(drone, lats[point], longs[point], 100, FAST_PARAM)
 
     # return home
     logging.info("Last waypoint reached")
@@ -90,5 +103,5 @@ if __name__ == "__main__":
         loop = asyncio.get_event_loop()
         loop.run_until_complete(run())
     except KeyboardInterrupt:
-        print("Program ended")
+        logging.info("CTRL+C: Program ended")
         sys.exit(0)
