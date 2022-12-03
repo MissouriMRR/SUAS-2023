d6867f6562edbc3f67c37f0b6d815afe87d7f3ed
Sean-Walsh-496 <85361337+Sean-Walsh-496@users.noreply.github.com>
2022-12-03 12:59:28 -0600

    passes mypy, pylint

--- flight/probability/helper.py
+++ flight/probability/helper.py
@@ -5,30 +5,14 @@ Several useful functions repeatedly used in other files
 """
 from math import sqrt
 
-TEST_AREA: list[tuple[float, float]] = [
-    (38.31722979755967,-76.5570186342245),
-    (38.3160801028265,-76.55731984244503),
-    (38.31600059675041,-76.5568902018946),
-    (38.31546739500083,-76.5537620127769),
-    (38.31470980862425,-76.5493636141453),
-    (38.31424154692598,-76.5466276164690),
-    (38.31369801280048,-76.5434238005822),
-    (38.3131406794544,-76.54011767488228),
-    (38.31508631356025,-76.5396286507867),
-    (38.31615083692682,-76.5449773879351),
-    (38.31734210679102,-76.5446085046679),
-    (38.31859044679581,-76.5519329158383),
-    (38.3164700703248,-76.55255360208943),
-    (38.31722979755967,-76.5570186342245)
-]
-
 AIR_DROP_AREA: list[tuple[float, float]] = [
     (38.31442311312976, -76.54522971451763),
     (38.31421041772561, -76.54400246436776),
     (38.3144070396263, -76.54394394383165),
     (38.31461622313521, -76.54516993186949),
-    (38.31442311312976, -76.54522971451763)
+    (38.31442311312976, -76.54522971451763),
 ]
+
 
 def calculate_dist(p_1: tuple[float, float], p_2: tuple[float, float]) -> float:
     """
@@ -46,7 +30,8 @@ def calculate_dist(p_1: tuple[float, float], p_2: tuple[float, float]) -> float:
     distance: float
         The Euclidian distance between these two points
     """
-    return sqrt((p_1[0] - p_2[0])**2 + (p_1[1] - p_2[1])**2)
+    return sqrt((p_1[0] - p_2[0]) ** 2 + (p_1[1] - p_2[1]) ** 2)
+
 
 def get_bounds(points: list[tuple[float, float]]) -> dict[str, list[float]]:
     """
@@ -66,16 +51,14 @@ def get_bounds(points: list[tuple[float, float]]) -> dict[str, list[float]]:
     x_bounds: list[float] = [float("inf"), float("-inf")]
     y_bounds: list[float] = [float("inf"), float("-inf")]
 
+    i: int
+    dim: tuple[int, list[float]]
     for i, _ in enumerate(points):
         for dim in ((0, x_bounds), (1, y_bounds)):
-            if points[i][dim[0]] < dim[1][0]: #smallest x | y
+            if points[i][dim[0]] < dim[1][0]:  # smallest x | y
                 dim[1][0] = points[i][dim[0]]
 
-            elif points[i][dim[0]] > dim[1][1]: #biggest x | y
+            elif points[i][dim[0]] > dim[1][1]:  # biggest x | y
                 dim[1][1] = points[i][dim[0]]
 
-    return {'x': x_bounds, 'y': y_bounds}
-
-if __name__ == "__main__":
-    x = get_bounds(TEST_AREA)
-    print(x['x'][1] - x['x'][0])
+    return {"x": x_bounds, "y": y_bounds}
