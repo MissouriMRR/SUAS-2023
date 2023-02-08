de6313bed43a6ba3c4c3de3f54ca270b6c01da28
GHJohnson8 <GHJohnson8@gmail.com>
2023-02-07 19:27:49 -0600

    The JSON File and JSON File Reader

---
+++ competition_inputs/BottleJSONReader.py
@@ -0,0 +1,11 @@
+import json
+# GJ 2-7-2023
+# Opens and imports JSON File 'BottleJSON.json' as a list
+f = open('competition_inputs/BottleJSON.json')
+data=json.load(f)
+
+# Defines Array of Bottles from the list from the JSON file
+Bottles=[data['Bottle1'][0], data['Bottle2'][0], data['Bottle3'][0], data['Bottle4'][0], data['Bottle5'][0]]
+
+# Closes the Json File
+f.close()
