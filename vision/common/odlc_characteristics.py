a2cfa6e031857971bab8f46f3cf61db20b1f2bbd
Cameron Falls <camfalls0201@gmail.com>
2023-02-14 15:06:36 -0600

    Create ranges for colors

--- vision/common/odlc_characteristics.py
+++ vision/common/odlc_characteristics.py
@@ -4,6 +4,10 @@ Contains common objects/classes for use in the classification of the standard OD
 
 from enum import Enum
 
+import numpy as np
+
+from nptyping import NDArray, Shape, UInt8
+
 
 class ODLCShapes(str, Enum):
     """
@@ -40,3 +44,20 @@ class ODLCColors(str, Enum):
     PURPLE = "Purple"
     BROWN = "Brown"
     ORANGE = "Orange"
+
+
+# Ranges of the colors in HSV color space
+COLOR_RANGES: dict[ODLCColors, NDArray[Shape["*, 2, 3"], UInt8]] = {
+    ODLCColors.WHITE: np.array([[[180, 18, 255], [0, 0, 231]]]),
+    ODLCColors.BLACK: np.array([[[180, 255, 30], [0, 0, 0]]]),
+    ODLCColors.GRAY: np.array([[[180, 18, 230], [0, 0, 40]]]),
+    ODLCColors.RED: np.array(
+        [[[180, 255, 255], [159, 50, 70]], [[9, 255, 255], [0, 50, 70]]]
+    ),  # red wraps around and needs 2 ranges
+    ODLCColors.BLUE: np.array([[[128, 255, 255], [90, 50, 70]]]),
+    ODLCColors.GREEN: np.array([[[89, 255, 255], [36, 50, 70]]]),
+    ODLCColors.YELLOW: np.array([[[35, 255, 255], [25, 50, 70]]]),
+    ODLCColors.PURPLE: np.array([[[158, 255, 255], [129, 50, 70]]]),
+    ODLCColors.BROWN: np.array([[[20, 255, 180], [10, 100, 120]]]),
+    ODLCColors.ORANGE: np.array([[[24, 255, 255], [10, 50, 70]]]),
+}
