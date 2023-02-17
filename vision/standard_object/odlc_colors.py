6985b57630c0d5cc69f1a2ad8f4d418f0067c9bd
Cameron Falls <camfalls0201@gmail.com>
2023-02-17 12:11:12 -0600

    Crop image function

--- vision/standard_object/odlc_colors.py
+++ vision/standard_object/odlc_colors.py
@@ -32,6 +32,7 @@ def find_colors(image: Image, text_bounds: BoundingBox) -> NDArray[Shape["2"], O
             the color of the text on the object
     """
     # slice image around bounds of text
+    cropped_img = crop_image(image, text_bounds)
 
     # kmeans clustering with k=2
 
@@ -43,3 +44,32 @@ def find_colors(image: Image, text_bounds: BoundingBox) -> NDArray[Shape["2"], O
 
     # return the 2 colors
     pass
+
+
+def crop_image(image: Image, bounds: BoundingBox) -> Image:
+    """
+    Crop an image around the given bounds. Will slice around
+    the bound's extremes as an upright rectangle.
+
+    Parameters
+    ----------
+    image : Image
+        the image to slice
+    bounds : BoundingBox
+        the bounds to slice from the image
+
+    Returns
+    -------
+    cropped_img : Image
+        the image cropped around the given bounds
+    """
+    min_y: int
+    max_y: int
+    min_y, max_y = bounds.get_y_extremes()
+
+    min_x: int
+    max_x: int
+    min_x, max_x = bounds.get_x_extremes()
+
+    cropped_img: Image = image[min_y:max_y, min_x, max_x, :]
+    return cropped_img
