f8c9a413cd23ea3dc048466fdfcc13229f9fa652
Cameron Falls <camfalls0201@gmail.com>
2023-02-17 12:34:35 -0600

    Initial kmeans function

--- vision/standard_object/odlc_colors.py
+++ vision/standard_object/odlc_colors.py
@@ -2,8 +2,10 @@
 Functions relating to finding the colors of the text and shape on the standard ODLC objects.
 """
 
+import cv2
 import numpy as np
-from nptyping import NDArray, Shape, UInt8
+
+from nptyping import NDArray, Shape, UInt8, Float32
 
 from vision.common.bounding_box import BoundingBox
 from vision.common.constants import Image
@@ -73,3 +75,62 @@ def crop_image(image: Image, bounds: BoundingBox) -> Image:
 
     cropped_img: Image = image[min_y:max_y, min_x, max_x, :]
     return cropped_img
+
+
+def run_kmeans(cropped_img: Image) -> Image:
+    """
+    Run kmeans clustering on the cropped image with K=2.
+
+    Assumption: the bounds around the text will contain 2 primary colors,
+    the text color and the shape color. Clustering with K=2 should result
+    in these colors being chosen as the unique colors.
+
+    NOTE: kmeans clustering performed on RGBXY data
+
+    Parameters
+    ----------
+    cropped_img : Image
+        the image cropped around the text
+
+    Returns
+    -------
+    kmeans_img : Image
+        the image after performing kmeans clustering with K=2,
+        will contain 2 unique colors
+    """
+    # preprocess image to make text/shape bigger, reducing the likelihood
+    # of kmeans choosing a ground color if bounds are loose
+    blurred_img: Image = cv2.medianBlur(cropped_img, ksize=9)
+
+    kernel: NDArray[Shape["5, 5"], UInt8]  # 5x5 kernal for use in dilation/erosion
+    eroded_img: Image = cv2.erode(blurred_img, kernel=kernel, iterations=1)
+    dilated_img: Image = cv2.dilate(eroded_img, kernel=kernel, iterations=1)
+
+    # convert to RGBXY
+    flattened: NDArray[Shape["*, 3"], UInt8] = dilated_img.reshape((-1, 3))
+    idxs: NDArray[Shape["*, 2"], UInt8] = np.array(
+        [idx for idx, _ in np.ndenumerate(np.mean(dilated_img, axis=2))]
+    )
+    vectorized: NDArray[Shape["*, 5"], UInt8] = np.hstack((flattened, idxs))  # RGBXY vector
+
+    # run kmeans with K=2
+    term_crit: tuple[int, int, float] = (
+        cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER,
+        10,
+        1.0,
+    )
+    k_val: int = 2
+
+    label: NDArray[str]
+    center: NDArray[Float32]
+    _, label, center = cv2.kmeans(
+        np.float32(vectorized), K=k_val, bestLabels=None, criteria=term_crit, attempts=10, flags=0
+    )
+
+    center_int: NDArray[UInt8] = center.astype(np.uint8)[:, :3]
+
+    # Convert back to BGR
+    kmeans_img: Image = center_int[label.flatten()]
+    kmeans_img = kmeans_img.reshape((dilated_img.shape))
+
+    return kmeans_img
