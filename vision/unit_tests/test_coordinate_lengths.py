5ead1c9978bf95341d6d0b2e0091226347245d04
xoc <jdwilcox32@gmail.com>
2023-03-09 20:04:44 -0600

    Changes made to pass pre-commit checks

--- vision/unit_tests/test_coordinate_lengths.py
+++ vision/unit_tests/test_coordinate_lengths.py
@@ -10,95 +10,142 @@ from vision.deskew.coordinate_lengths import latitude_length
 from vision.deskew.coordinate_lengths import longitude_length
 
 
-
 class TestCoordinates(unittest.TestCase):
-    #Testing latitude_length
+    """
+    Testing the coordinate lengths module
+    """
 
+    # Testing latitude_length
 
-    #Tests the maximum possible latitude in degrees as an input to the latitude_length function
-    def test_latitude_length_at_max_val(self):
-        MAX_INPUT_IN_DEGREES = 90 
-        MAX_INPUT_IN_RADIANS: float = np.deg2rad(MAX_INPUT_IN_DEGREES)
-        LATITUDE_LENGTH_AT_MAX_LATITUDE: float = ( 
-            111132.92 
-            - 559.82 * np.cos(2 * MAX_INPUT_IN_RADIANS) 
-            + 1.175 * np.cos(4 * MAX_INPUT_IN_RADIANS) 
-            - 0.0023 * np.cos(6 * MAX_INPUT_IN_RADIANS) 
+    # Tests the maximum possible latitude in degrees as an input to the latitude_length function
+    def test_latitude_length_at_max_val(self) -> None:
+        """
+        Asserts that latitude_length returns expected value at maximum input (90 degrees)
+
+        Returns
+        -------
+        None
+        """
+        max_input_in_degrees = 90
+        max_input_in_radians: float = np.deg2rad(max_input_in_degrees)
+        latitude_length_at_max_latitude: float = (
+            111132.92
+            - 559.82 * np.cos(2 * max_input_in_radians)
+            + 1.175 * np.cos(4 * max_input_in_radians)
+            - 0.0023 * np.cos(6 * max_input_in_radians)
         )
-        FUNCTION_RETURN_AT_MAX_LATITUDE = latitude_length(MAX_INPUT_IN_DEGREES)
+        function_return_at_max_latitude = latitude_length(max_input_in_degrees)
         # Tests to insure the expected output is the same as the actual output, given the same input
-        self.assertEqual(LATITUDE_LENGTH_AT_MAX_LATITUDE, FUNCTION_RETURN_AT_MAX_LATITUDE)
-    
-    #Tests the minimum possible latitude in degrees as an input to the latitude_length function
-    def test_latitude_length_min_val(self):
-        MIN_INPUT_IN_DEGREES = -90
-        MIN_INPUT_IN_RADIANS: float = np.deg2rad(MIN_INPUT_IN_DEGREES)
-        LATITUDE_LENGTH_AT_MIN_LATITUDE: float = (
-            111132.92 
-            - 559.82 * np.cos(2 * MIN_INPUT_IN_RADIANS)
-            + 1.175 * np.cos(4 * MIN_INPUT_IN_RADIANS)
-            - 0.0023 * np.cos(6 * MIN_INPUT_IN_RADIANS) 
+        self.assertEqual(latitude_length_at_max_latitude, function_return_at_max_latitude)
+
+    # Tests the minimum possible latitude in degrees as an input to the latitude_length function
+    def test_latitude_length_min_val(self) -> None:
+        """
+        Asserts that latitude_length returns expected value at minimum input (-90 degrees)
+
+        Returns
+        -------
+        None
+        """
+        max_input_in_degrees = -90
+        min_input_in_radians: float = np.deg2rad(max_input_in_degrees)
+        latitude_length_at_min_latitude: float = (
+            111132.92
+            - 559.82 * np.cos(2 * min_input_in_radians)
+            + 1.175 * np.cos(4 * min_input_in_radians)
+            - 0.0023 * np.cos(6 * min_input_in_radians)
         )
-        FUNCTION_RETURN_AT_MIN_LATITUDE = latitude_length(MIN_INPUT_IN_DEGREES)
+        function_return_at_min_latitude = latitude_length(max_input_in_degrees)
         # Tests to insure the expected output is the same as the actual output, given the same input
-        self.assertEqual(LATITUDE_LENGTH_AT_MIN_LATITUDE, FUNCTION_RETURN_AT_MIN_LATITUDE)
+        self.assertEqual(latitude_length_at_min_latitude, function_return_at_min_latitude)
 
-    #Tests a basic input in degrees as an input to the latitude_length function
-    def test_latitude_length_at_zero(self):
-        BASIC_INPUT_IN_DEGREES = 0
-        BASIC_INPUT_IN_RADIANS: float = np.deg2rad(BASIC_INPUT_IN_DEGREES)
-        LATITUDE_LENGTH_AT_ZERO_DEGREES_LATITUDE: float = (
-            111132.92 
-            - 559.82 * np.cos(2 * BASIC_INPUT_IN_RADIANS) 
-            + 1.175 * np.cos(4 * BASIC_INPUT_IN_RADIANS)
-            - 0.0023 * np.cos(6 * BASIC_INPUT_IN_RADIANS) 
+    # Tests a basic input in degrees as an input to the latitude_length function
+    def test_latitude_length_at_zero(self) -> None:
+        """
+        Asserts that latitude_length returns expected value at a basic input (0 degrees)
+
+        Returns
+        -------
+        None
+        """
+        basic_input_in_degrees = 0
+        basic_input_in_radians: float = np.deg2rad(basic_input_in_degrees)
+        latitude_length_at_zero_degrees_latitude: float = (
+            111132.92
+            - 559.82 * np.cos(2 * basic_input_in_radians)
+            + 1.175 * np.cos(4 * basic_input_in_radians)
+            - 0.0023 * np.cos(6 * basic_input_in_radians)
         )
-        FUNCTION_RETURN_AT_ZERO_DEGREES_LATITUDE = latitude_length(BASIC_INPUT_IN_RADIANS)
+        function_return_at_zero_degrees_latitude = latitude_length(basic_input_in_radians)
         # Tests to insure the expected output is the same as the actual output, given the same input
-        self.assertEqual(LATITUDE_LENGTH_AT_ZERO_DEGREES_LATITUDE, FUNCTION_RETURN_AT_ZERO_DEGREES_LATITUDE)
-
-    #Testing longitude_length
-
-
-    #Tests the maximum possible latitude in degrees as an input to the latitude_length function
-    def test_longitude_length_at_max_val(self):
-        MAX_INPUT_IN_DEGREES = 90 
-        MAX_INPUT_IN_RADIANS: float = np.deg2rad(MAX_INPUT_IN_DEGREES)
-        LONGITUDE_LENGTH_AT_MAX_LATITUDE: float = ( 
-            111412.84 * np.cos(MAX_INPUT_IN_RADIANS)
-            - 93.5 * np.cos(3 * MAX_INPUT_IN_RADIANS)
-            + 0.118 * np.cos(5 * MAX_INPUT_IN_RADIANS)
+        self.assertEqual(
+            latitude_length_at_zero_degrees_latitude, function_return_at_zero_degrees_latitude
         )
-        FUNCTION_RETURN_AT_MAX_LATITUDE = longitude_length(MAX_INPUT_IN_DEGREES)
-        # Tests to insure the expected output is the same as the actual output, given the same input
-        self.assertEqual(LONGITUDE_LENGTH_AT_MAX_LATITUDE, FUNCTION_RETURN_AT_MAX_LATITUDE)
-    
-    #Tests the minimum possible latitude in degrees as an input to the latitude_length function
-    def test_longitude_length_min_val(self):
-        MIN_INPUT_IN_DEGREES = -90
-        MIN_INPUT_IN_RADIANS: float = np.deg2rad(MIN_INPUT_IN_DEGREES)
-        LONGITUDE_LENGTH_AT_MIN_LATITUDE: float = (
-            111412.84 * np.cos(MIN_INPUT_IN_RADIANS)
-            - 93.5 * np.cos(3 * MIN_INPUT_IN_RADIANS)
-            + 0.118 * np.cos(5 * MIN_INPUT_IN_RADIANS)
+
+    # Testing longitude_length
+
+    # Tests the maximum possible latitude in degrees as an input to the latitude_length function
+    def test_longitude_length_at_max_val(self) -> None:
+        """
+        Asserts that longitude_length returns expected value at maximum input (90 degrees)
+
+        Returns
+        -------
+        None
+        """
+        max_input_in_degrees = 90
+        max_input_in_radians: float = np.deg2rad(max_input_in_degrees)
+        longitude_length_at_max_latitude: float = (
+            111412.84 * np.cos(max_input_in_radians)
+            - 93.5 * np.cos(3 * max_input_in_radians)
+            + 0.118 * np.cos(5 * max_input_in_radians)
         )
-        FUNCTION_RETURN_AT_MIN_LATITUDE = longitude_length(MIN_INPUT_IN_DEGREES)
+        function_return_at_max_latitude = longitude_length(max_input_in_degrees)
         # Tests to insure the expected output is the same as the actual output, given the same input
-        self.assertEqual(LONGITUDE_LENGTH_AT_MIN_LATITUDE, FUNCTION_RETURN_AT_MIN_LATITUDE)
+        self.assertEqual(longitude_length_at_max_latitude, function_return_at_max_latitude)
 
-    #Tests a basic input in degrees as an input to the latitude_length function
-    def test_longitude_length_at_zero(self):
-        BASIC_INPUT_IN_DEGREES = 0
-        BASIC_INPUT_IN_RADIANS: float = np.deg2rad(BASIC_INPUT_IN_DEGREES)
-        LONGITUDE_LENGTH_AT_ZERO_DEGREES_LATITUDE: float = (
-            111412.84 * np.cos(BASIC_INPUT_IN_RADIANS)
-            - 93.5 * np.cos(3 * BASIC_INPUT_IN_RADIANS)
-            + 0.118 * np.cos(5 * BASIC_INPUT_IN_RADIANS)
+    # Tests the minimum possible latitude in degrees as an input to the latitude_length function
+    def test_longitude_length_min_val(self) -> None:
+        """
+        Asserts that longitude_length returns expected value at minimum input (-90 degrees)
+
+        Returns
+        -------
+        None
+        """
+        max_input_in_degrees = -90
+        min_input_in_radians: float = np.deg2rad(max_input_in_degrees)
+        longitude_length_at_min_latitude: float = (
+            111412.84 * np.cos(min_input_in_radians)
+            - 93.5 * np.cos(3 * min_input_in_radians)
+            + 0.118 * np.cos(5 * min_input_in_radians)
         )
-        FUNCTION_RETURN_AT_ZERO_DEGREES_LATITUDE = longitude_length(BASIC_INPUT_IN_RADIANS)
+        function_return_at_min_latitude = longitude_length(max_input_in_degrees)
         # Tests to insure the expected output is the same as the actual output, given the same input
-        self.assertEqual(LONGITUDE_LENGTH_AT_ZERO_DEGREES_LATITUDE, FUNCTION_RETURN_AT_ZERO_DEGREES_LATITUDE)
+        self.assertEqual(longitude_length_at_min_latitude, function_return_at_min_latitude)
+
+    # Tests a basic input in degrees as an input to the latitude_length function
+    def test_longitude_length_at_zero(self) -> None:
+        """
+        Asserts that longitude_length returns expected value at a basic input (0 degrees)
+
+        Returns
+        -------
+        None
+        """
+        basic_input_in_degrees = 0
+        basic_input_in_radians: float = np.deg2rad(basic_input_in_degrees)
+        longitude_length_at_zero_degrees_latitude: float = (
+            111412.84 * np.cos(basic_input_in_radians)
+            - 93.5 * np.cos(3 * basic_input_in_radians)
+            + 0.118 * np.cos(5 * basic_input_in_radians)
+        )
+        function_return_at_zero_degrees_latitude = longitude_length(basic_input_in_radians)
+        # Tests to insure the expected output is the same as the actual output, given the same input
+        self.assertEqual(
+            longitude_length_at_zero_degrees_latitude, function_return_at_zero_degrees_latitude
+        )
 
 
-if __name__ == '__main__':
-    unittest.main() #add "verbosity=2" in the parameters to main here for more detailed output
+if __name__ == "__main__":
+    unittest.main()  # add "verbosity=2" in the parameters to main here for more detailed output
