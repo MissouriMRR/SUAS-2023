865b90f801689d4eb2c4cfc2d0416bf542f7f0cb
Davis <djdmvp@umsystem.edu>
2023-03-14 19:57:52 -0500

    odlc pathing state code with comments flight/states/odlcs.py$

--- flight/states/odlcs.py
+++ flight/states/odlcs.py
@@ -3,7 +3,9 @@
 from mavsdk import System
 from flight.states.state import State
 from flight.states.airdrop import AirDrop
-
+from flight.odlc_boundaries.execute import move_to
+from mavsdk.camera import Mode
+import logging
 
 class ODLC(State):
     """
@@ -29,4 +31,28 @@ class ODLC(State):
         AirDrop : State
             The next state in the state machine, AirDrop for the payloads
         """
+
+        #Camera gets turned on
+        await drone.camera.set_mode(Mode.PHOTO)
+        logging.info("Camera changed to Photo mode")
+
+        #These waypoint values are all that are needed to traverse the whole odlc drop location because it is a small rectangle
+        #The first waypoint is the midpoint of the left side of the rectangle(one of the short sides), the second point is the
+        #midpoint of the right side of the rectangle(other short side), and the third point is the top left corner of the rectangle
+        #it goes there for knowing where the drone ends to travel to each of the drop locations, the altitude is locked at 85 because
+        #we want the drone to stay level and the camera to view the whole odlc boundary
+        waypoint: dict[str, list[float]] = {
+        "lats": [38.31451966813249, 38.31430872867596, 38.31461622313521],
+        "longs": [-76.54519982319357, -76.54397320409971, -76.54516993186949],
+        "Altitude": [85],
+        }
+
+        #traverses the 3 waypoints starting at the midpoint on left to midpoint on the right then to the top left corner at the rectangle
+        point: int
+        for point in range(3):
+            logging.info("Moving")
+            await move_to(
+                drone, waypoint["lats"][point], waypoint["longs"][point], waypoint["Altitude"][0]
+            )
+
         return AirDrop(self.state_settings)
