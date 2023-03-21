273a274330c14b310de697bd442e0b5298462625
Ooglely <aahgr@mst.edu>
2023-03-21 18:24:40 -0500

    Finish maestro class documentation

--- flight/maestro/maestro.py
+++ flight/maestro/maestro.py
@@ -1,22 +1,60 @@
-import serial
+"""
+File containing the Maestro class to be able to control the Maestro board
+"""
+
 import time
+import serial
 
 
 class Maestro:
     """
-    Array with associated photographic information.
+    Maestro class to control the Maestro board
 
     Attributes
     ----------
-    exposure : float
-        Exposure in seconds.
+    serial: serial.Serial
+        Serial object to be able to read and send messages to and from the board.
+
+    command_prefix: str
+        The command protocol prefix that is sent before every command to the board.
+
+    targets: list(int)
+        A list of the current targets for each channel.
 
     Methods
     -------
-    colorspace(c='rgb')
-        Represent the photo in the given colorspace.
-    gamma(n=1.0)
-        Change the photo's gamma exposure.
+    _send_command(command: str) -> None
+        Sends a serial command to the board
+
+    _get_bits(integer: int) -> tuple[int, int]
+        Converts an integer value to the 7 high bit, 7 low bit format that the Pololu commands use
+
+    _read_bits() -> int
+        Read a 16 bit response from the Maestro board and convert it to an integer
+
+    set_target(channel: int, target: int) -> None
+        Sets the target position of the specified servo
+
+    set_speed(channel: int, speed: int) -> None
+        Sets the speed limit of the desired servo
+
+    set_acceleration(channel: int, accel: int) -> None
+        Sets the target position of the specified servo
+
+    get_position(channel: int) -> int
+        Returns the current position of the servo
+
+    get_moving_state() -> bool
+        States if any servos are currently moving
+
+    getError() -> bool
+        States if there is an error with the board
+
+    go_home() -> None
+        Sets all servos to their home position
+
+    is_moving() -> bool
+        Checks if a certain servo is still moving
     """
 
     def __init__(
@@ -39,11 +77,13 @@ class Maestro:
         baud_rate: int
             The rate of communication configured for the board (default 9600)
         """
-        self.serial = serial.Serial(port=connection_address, baudrate=baud_rate, timeout=0.050)
-        self.commandPrefix = chr(0xAA) + chr(device_number)
-        self.targets = [-1] * 24
+        self.serial: serial.Serial = serial.Serial(
+            port=connection_address, baudrate=baud_rate, timeout=0.050
+        )
+        self.command_prefix: str = chr(0xAA) + chr(device_number)
+        self.targets: list[int] = [-1] * 24
 
-    def _sendCommand(self, command: str) -> None:
+    def _send_command(self, command: str) -> None:
         """
         Sends a serial command to the board
 
@@ -52,9 +92,9 @@ class Maestro:
         command: str
             The command (in bits) to be sent to the board
         """
-        self.serial.write(bytes(self.commandPrefix + command, "latin-1"))
+        self.serial.write(bytes(self.command_prefix + command, "latin-1"))
 
-    def _getBits(self, integer: int) -> tuple[int, int]:
+    def _get_bits(self, integer: int) -> tuple[int, int]:
         """
         Converts an integer value to the 7 high bit, 7 low bit format that the Pololu commands use
         See: https://www.pololu.com/docs/0J40/5.e
@@ -72,11 +112,11 @@ class Maestro:
             high_bits: int
                 The 7 high bits of the integer
         """
-        low_bits: str = integer & 0x7F
-        high_bits: str = (integer >> 7) & 0x7F
+        low_bits: int = integer & 0x7F
+        high_bits: int = (integer >> 7) & 0x7F
         return low_bits, high_bits
 
-    def _readBits(self) -> int:
+    def _read_bits(self) -> int:
         """
         Read a 16 bit response from the Maestro board and convert it to an integer
 
@@ -89,7 +129,7 @@ class Maestro:
         msb = ord(self.serial.read())
         return (msb << 8) + lsb
 
-    def setTarget(self, channel: int, target: int) -> None:
+    def set_target(self, channel: int, target: int) -> None:
         """
         Sets the target position of the specified servo
 
@@ -107,13 +147,13 @@ class Maestro:
         To set the servos to full right, set the target to 8000.
         The range of the servo target is 4000 - 8000.
         """
-        low, high = self._getBits(target)
+        low, high = self._get_bits(target)
         command = chr(0x04) + chr(channel) + chr(low) + chr(high)
 
-        self._sendCommand(command)
+        self._send_command(command)
         self.targets[channel] = target
 
-    def setSpeed(self, channel: int, speed: int) -> None:
+    def set_speed(self, channel: int, speed: int) -> None:
         """
         Sets the speed limit of the desired servo
 
@@ -125,12 +165,12 @@ class Maestro:
         speed: int
             The speed limit to set the channel to in units of quarter-microseconds/10 ms
         """
-        low, high = self._getBits(speed)
+        low, high = self._get_bits(speed)
 
         command = chr(0x07) + chr(channel) + chr(low) + chr(high)
-        self._sendCommand(command)
+        self._send_command(command)
 
-    def setAcceleration(self, channel: int, accel: int) -> None:
+    def set_acceleration(self, channel: int, accel: int) -> None:
         """
         Sets the target position of the specified servo
 
@@ -147,12 +187,12 @@ class Maestro:
         The range for acceleration is from 0 to 255.
         0 indicates no acceleration limit.
         """
-        low, high = self._getBits(accel)
+        low, high = self._get_bits(accel)
 
         command = chr(0x09) + chr(channel) + chr(low) + chr(high)
-        self._sendCommand(command)
+        self._send_command(command)
 
-    def getPosition(self, channel: int) -> int:
+    def get_position(self, channel: int) -> int:
         """
         Returns the current position of the servo
 
@@ -167,12 +207,12 @@ class Maestro:
             The current position of the servo in quarter-microseconds
         """
         command = chr(0x10) + chr(channel)
-        self._sendCommand(command)
-        result = self._readBits()
+        self._send_command(command)
+        result = self._read_bits()
         print(result)
         return result
 
-    def getMovingState(self) -> bool:
+    def get_moving_state(self) -> bool:
         """
         States if any servos are currently moving
 
@@ -185,13 +225,13 @@ class Maestro:
         -----
         Only works with Maestro 12, 18, 24
         """
-        self._sendCommand(chr(0x13))
+        self._send_command(chr(0x13))
         response = self.serial.read()
         if response == 0x01:
             return True
         return False
 
-    def getErrors(self) -> int:
+    def get_errors(self) -> int:
         """
         Returns the error code if there are any
 
@@ -200,18 +240,18 @@ class Maestro:
         error: int
             The error code
         """
-        self._sendCommand(chr(0x21))
-        result = self._readBits()
+        self._send_command(chr(0x21))
+        result = self._read_bits()
         print(result)
         return result
 
-    def goHome(self) -> None:
+    def go_home(self) -> None:
         """
         Sends all servos and outputs to their home positions
         """
-        self._sendCommand(chr(0x22))
+        self._send_command(chr(0x22))
 
-    def isMoving(self, channel) -> bool:
+    def is_moving(self, channel: int) -> bool:
         """
         Checks if a certain servo is still moving
 
@@ -226,22 +266,22 @@ class Maestro:
             True if the servo is still moving, false if not
         """
         if self.targets[channel] != -1:
-            if self.getPosition(channel) != self.targets[channel]:
+            if self.get_position(channel) != self.targets[channel]:
                 return True
         return False
 
 
 if __name__ == "__main__":
     testMaestro = Maestro()
-    testMaestro.setTarget(0, 4000)
-    testMaestro.setTarget(1, 4000)
-    testMaestro.setTarget(2, 4000)
-    testMaestro.setTarget(9, 4000)
-    testMaestro.setTarget(10, 4000)
-    testMaestro.getPosition(0)
+    testMaestro.set_target(0, 4000)
+    testMaestro.set_target(1, 4000)
+    testMaestro.set_target(2, 4000)
+    testMaestro.set_target(9, 4000)
+    testMaestro.set_target(10, 4000)
+    testMaestro.get_position(0)
     time.sleep(2)
-    testMaestro.setTarget(0, 8000)
-    testMaestro.setTarget(1, 8000)
-    testMaestro.setTarget(2, 8000)
-    testMaestro.setTarget(9, 8000)
-    testMaestro.setTarget(10, 8000)
+    testMaestro.set_target(0, 8000)
+    testMaestro.set_target(1, 8000)
+    testMaestro.set_target(2, 8000)
+    testMaestro.set_target(9, 8000)
+    testMaestro.set_target(10, 8000)
