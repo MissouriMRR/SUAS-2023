be3a5b66802eb02a5f71a05fd1ab5fd632fa7012
evan <evan.blake03@gmail.com>
2023-04-05 12:24:00 -0500

    Cleaned up after merge and added docstrings

--- vision/pipeline/pipeline_funcs.py
+++ vision/pipeline/pipeline_funcs.py
@@ -20,6 +20,27 @@ from vision.standard_object.odlc_colors import find_colors
 from vision.deskew.camera_distances import get_coordinates
 
 
+def read_parameter_json(json_path: str) -> dict[str, CameraParameters]:
+    """
+    Will read in the data from the given json file and return it as a python dict.
+
+    Parameters
+    ----------
+    json_path : str
+        The path of a valid json file, assumed to have data in the same format as return type.
+
+    Returns
+    -------
+    data : dict[str, CameraParameters]
+        The python dict version of the data from the given json file.
+    """
+    
+    with open(json_path, encoding="utf-8") as jfile:
+        data: dict[str, CameraParameters] = json.load(jfile)
+
+    return data
+
+
 def find_humanoids(
     emg_model: Callable[[Image], str],
     original_image: Image,
@@ -39,6 +60,11 @@ def find_humanoids(
         The details of how and where the photo was taken
     image_path: str
         The path for the image the bounding box is from
+    
+    Returns
+    -------
+    found_humanoids: list[BoundingBox]
+        The list of bounding boxes of detected humanoids
     """
 
     # The potential emergent objects found in the image
@@ -57,29 +83,27 @@ def find_humanoids(
     return found_humanoids
 
 
-def read_image_parameters(json_path: str) -> dict[str, CameraParameters]:
-    """
-    Will read in the data from the given json file and return it as a python dict.
-
-    Parameters
-    ----------
-    json_path : str
-        The path of a valid json file, assumed to have data in the same format as return type.
-
-    Returns
-    -------
-    data : dict[str, CameraParameters]
-        The python dict version of the data from the given json file.
-    """
-    with open(json_path, encoding="utf-8") as jfile:
-        data: dict[str, CameraParameters] = json.load(jfile)
-
-    return data
-
-
 def find_standard_objects(
     original_image: Image, camera_parameters: CameraParameters, image_path: str
 ) -> list[BoundingBox]:
+    """
+    Finds all bounding boxes of standard objects in an image
+    
+    Parameters
+    ----------
+    original_image: Image
+        The image to find shapes in
+    camera_parameters: CameraParameters
+        The details of how and where the photo was taken
+    image_path: str
+        The path for the image the bounding box is from
+    
+    Returns
+    -------
+    found_odlcs: list[BoundingBox]
+        The list of bounding boxes of detected standard objects
+    """
+    
     found_odlcs: list[BoundingBox] = []
 
     # Run the image preprocessing
@@ -157,17 +181,38 @@ def set_generic_attributes(
     image_shape: tuple[int, int] | tuple[int, int, int],
     camera_parameters: CameraParameters,
 ) -> None:
+    """
+    Sets BoundingBox attributes by reference. Attributes changed are image_path, latitude, and longitude
+    "Generic" because these attributes are important for any object
+    
+    Parameters
+    ----------
+    object: BoundingBox
+        The bounding box of the object to which the attributes will be set
+    image_path: str
+        The path for the image the bounding box is from
+    image_shape : tuple[int, int, int] | tuple[int, int]
+        The shape of the image (returned by `image.shape` when image is a numpy image array)
+    camera_parameters: CameraParameters
+        The details of how and where the photo was taken
+    
+    Returns
+    -------
+    attributes_found: bool
+        Returns true if all attributes were successfully found
+    """
+    
     object.set_attribute("image_path", image_path)
 
     coordinates: tuple[float, float] | None = get_coordinates(
         object.get_center_coord(), image_shape, camera_parameters
     )
 
-    object.set_attribute("latitude", coordinates[0])
-    object.set_attribute("longitude", coordinates[1])
-
     if coordinates is None:
         return False
+    
+    object.set_attribute("latitude", coordinates[0])
+    object.set_attribute("longitude", coordinates[1])
     
     return True
 
