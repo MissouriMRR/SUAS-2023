188056e8f12ef1c1e7bad6061e5d90015f5f79be
evan <evan.blake03@gmail.com>
2023-04-20 18:29:18 -0500

    Added docstrings and fixed variables

--- vision/airdrop_vision_pipeline.py
+++ vision/airdrop_vision_pipeline.py
@@ -17,16 +17,32 @@ import vision.pipeline.pipeline_utils as pipe_utils
 # The furthest a judge can move between images in feet
 MAX_JUDGE_MOVEMENT_F: float = 15
 
+
 def airdrop_pipeline(camera_data_path: str, state_path: str, output_path: str) -> None:
     """
-    
+    Finds all judges in the images in the camera_data_path json file.
+
+    Unlike with the Flyover Vision Pipeline, the output_path json will be updated continuously.
+
+    Parameters
+    ----------
+    camera_data_path: str
+        The path to the json file containing the CameraParameters entries
+    state_path: str
+        A text file containing True if all images have been taken and False otherwise
+    output_path: str
+        The json file name and path to save the data in
     """
-    
+
     # Load model
     emg_model: Callable[[consts.Image], str] = create_emergent_model()
-    
+
     # List of filenames for images already completed to prevent repeating work
     completed_images: list[str] = []
+
+
+    prev_data: list[BoundingBox] = []
+    first_detection: bool = True
     
     # Wait for and process unfinished images until no more images are being taken
     all_images_taken: bool = False
@@ -41,8 +57,6 @@ def airdrop_pipeline(camera_data_path: str, state_path: str, output_path: str) -
         image_parameters: dict[str, consts.CameraParameters] = pipe_utils.read_parameter_json(
             camera_data_path
         )
-        
-        prev_data: list[BoundingBox]
 
         # Loop through all images in the json - if it hasn't been processed, process it
         for image_path in image_parameters.keys():
@@ -61,33 +75,71 @@ def airdrop_pipeline(camera_data_path: str, state_path: str, output_path: str) -
                     image, emg_model, camera_parameters, image_path
                 )
                 
-                judges: list[BoundingBox] = compare_data(prev_data, current_data)
-                
-                judge_dict: consts.ODLC_Dict = create_judge_dict(judges)
-                
-                pipe_utils.output_odlc_json(output_path, judge_dict)
-                
+                if not first_detection:
+                    judges: list[BoundingBox] = compare_data(prev_data, current_data)
+
+                    judge_dict: consts.ODLC_Dict = create_judge_dict(judges)
+
+                    pipe_utils.output_odlc_json(output_path, judge_dict)
+
                 prev_data = current_data
+                
+                first_detection = False
 
 
-def compare_data(prev_data: list[BoundingBox], current_data: list[BoundingBox]) -> list[BoundingBox]:
+def compare_data(
+    prev_data: list[BoundingBox], current_data: list[BoundingBox]
+) -> list[BoundingBox]:
+    """ "
+    Compares humanoid detections between two images to filter out false positive detections
+
+    Parameters
+    ----------
+    prev_data: list[BoundingBox]
+        The detected humanoids from the previous image being considered
+    current_data: list[BoundingBox]
+        The detected humanoids from the current image
+
+    Returns
+    -------
+    judges: list[BoundingBox]
+        The subset of current_data that is likely to be judges
+    """
+
     judges: list[BoundingBox] = []
-    
-    for prev_detection in prev_data:
-        min_distance = calc_emerg_obj_min_dist(current_data, prev_detection)
-        
+
+    for current_detection in current_data:
+        min_distance = calc_emerg_obj_min_dist(prev_data, current_detection)
+
+        # Ensures that a detection in the current image that was not in the previous image
+        #   will be ignored
         if min_distance < MAX_JUDGE_MOVEMENT_F:
-            judges.append(prev_detection)
-    
+            judges.append(current_detection)
+
     return judges
 
 
-def create_judge_dict(judges: list[BoundingBox]) -> None:
-    judge_dict: consts.ODLC_Dict
+def create_judge_dict(judges: list[BoundingBox]) -> consts.ODLC_Dict:
+    """
+    Creates an ODLC_Dict object that represents where the judges probably are
+
+    Parameters
+    ----------
+    judges: list[BoundingBox]
+        The list of bounding boxes of judges
+
+    Returns
+    -------
+    judge_dict: ODLC_Dict
+        The dictionary of locations of judges.
+        The keys are indices in string form and are meaningless.
+    """
+
+    judge_dict: consts.ODLC_Dict = {}
     for i, judge in enumerate(judges):
         judge_dict[str(i)] = {
-            "latitude": judge.get_attribute("latitude"), 
-            "longitude": judge.get_attribute("latitude")
+            "latitude": judge.get_attribute("latitude"),
+            "longitude": judge.get_attribute("latitude"),
         }
-    
+
     return judge_dict
