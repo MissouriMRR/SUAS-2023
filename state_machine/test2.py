452d0bc5ee142c4e3b5d1e8c092528dbcaebb38f
RexBerry <59031902+RexBerry@users.noreply.github.com>
2023-09-21 19:04:03 -0500

    Make test 2 good

--- state_machine/test2.py
+++ state_machine/test2.py
@@ -1,76 +1,48 @@
 """Tests the state machine."""
 
+import asyncio
+from multiprocessing import Process
+import time
+
 from .drone import Drone
 from .state_machine import StateMachine
 from .states import Start
-import asyncio
-from threading import Thread
-import multiprocessing
-import time
-async def actually_run_drone(drone: Drone):
+
+
+async def actually_run_drone(drone: Drone) -> None:
+    """Create and run a state machine machine for the drone."""
     print("testing")
     await StateMachine(Start(drone), drone).run()
 
-    
-def run_drone(drone: Drone):
+
+def run_drone(drone: Drone) -> None:
+    """Create and run a state machine in the event loop."""
     print("Running drone")
     asyncio.run(actually_run_drone(drone))
-    
-async def stop_states():
-    print("Stopping states")
-    await asyncio.sleep(1)
-    print("stopping now")
-    current_loop = asyncio.get_running_loop()
-    tasks = asyncio.all_tasks(current_loop)
-    for task in tasks:
-        print(task)
-        status = task.cancel()
-        print(status)
-    
-async def threat_test() -> None:
-    """Test the state machine."""
 
-    t = Thread(target=run_drone)
 
-    t.run()
-    
-    await asyncio.sleep(5)
-    print("stop it now!!")
-    # Create a coroutine
-    loop = asyncio.get_running_loop()
-    
-    print("1")
-    # Submit the coroutine to a given loop
-    asyncio.run_coroutine_threadsafe(stop_states(), loop)
-    print("2")
-    
-    while True:
-        await asyncio.sleep(5)
-    # Wait for the result with an optional timeout argument
-    #assert future.result(timeout=6run_drone)
-    
-def process_test():
-    drone_obj = Drone()
-    
+def process_test() -> None:
+    """Test running the state machine in a separate process."""
+    drone_obj: Drone = Drone()
+
     print("Starting processes")
-    state_machine = multiprocessing.Process(target=run_drone, args=(drone_obj, ))
-    kill_switch_process = multiprocessing.Process(target=kill_switch, args=(state_machine, ))
-    
+    state_machine: Process = Process(target=run_drone, args=(drone_obj,))
+    kill_switch_process: Process = Process(target=kill_switch, args=(state_machine,))
+
     state_machine.start()
     kill_switch_process.start()
-    
+
     state_machine.join()
     print("State machine joined")
     kill_switch_process.join()
     print("Kill switch joined")
-    
+
     print("Done!")
-    
-    
-def kill_switch(state_machine_process: multiprocessing.Process):
-    i = 0
-    while (i < 20):
+
+
+def kill_switch(state_machine_process: Process) -> None:
+    """Kill the state machine after approximately 20 seconds"""
+    for i in range(20):
         print(f"Kill switch is on cycle {i}")
         time.sleep(1)
-        i += 1
     state_machine_process.terminate()
--- test2.py
+++ test2.py
@@ -1,13 +1,8 @@
 """Tests the state machine."""
 
-import asyncio
 import logging
 
 from state_machine.test2 import process_test
 
 logging.basicConfig(level=logging.INFO)
 process_test()
-
-
-#asyncio.run(test()) for the other one
-
