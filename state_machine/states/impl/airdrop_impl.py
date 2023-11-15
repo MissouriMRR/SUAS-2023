9ffbac7bb90cf70aa49fda25a19ff7cb686fea2a
Davis <davis@UbuntuDavis.myguest.virtualbox.org>
2023-11-14 19:07:42 -0600

    rebase and updating the branch >o<

---
+++ state_machine/states/impl/airdrop_impl.py
@@ -0,0 +1,41 @@
+"""Implements the behavior of the Airdrop state."""
+import asyncio
+import logging
+
+from state_machine.states.airdrop import Airdrop
+from state_machine.states.waypoint import Waypoint
+from state_machine.states.state import State
+
+
+async def run(self: Airdrop) -> State:
+    """
+    Implements the run method for the Airdrop state.
+
+    This method initiates the Airdrop process of the drone and transitions to the Waypoint state.
+
+    Returns
+    -------
+    Waypoint : State
+        The next state after the drone has successfully completed the Airdrop.
+
+    Notes
+    -----
+    This method is responsible for initiating the Airdrop process of the drone and transitioning
+    it back to the Waypoint state.
+
+    """
+    try:
+        logging.info("Airdrop")
+
+        # code for Airdrop logic
+
+        return Waypoint(self.drone)
+    except asyncio.CancelledError as ex:
+        logging.error("Airdrop state canceled")
+        raise ex
+    finally:
+        pass
+
+
+# Setting the run_callable attribute of the Airdrop class to the run function
+Airdrop.run_callable = run
---
+++ state_machine/states/impl/odlc_impl.py
@@ -0,0 +1,40 @@
+"""Implements the behavior of the ODLC state."""
+import asyncio
+import logging
+
+from state_machine.states.airdrop import Airdrop
+from state_machine.states.odlc import ODLC
+from state_machine.states.state import State
+
+
+async def run(self: ODLC) -> State:
+    """
+    Implements the run method for the ODLC state.
+
+    This method initiates the ODLC process of the drone and transitions to the Airdrop state.
+
+    Returns
+    -------
+    Airdrop : State
+        The next state after the drone has successfully scanned the ODLC area.
+
+    Notes
+    -----
+    This method is responsible for initiating the ODLC scanning process of the drone
+    and transitioning it to the Airdrop state.
+
+    """
+    try:
+        logging.info("ODLC")
+        # code for the ODLC logic
+
+        return Airdrop(self.drone)
+    except asyncio.CancelledError as ex:
+        logging.error("ODLC state canceled")
+        raise ex
+    finally:
+        pass
+
+
+# Setting the run_callable attribute of the ODLC class to the run function
+ODLC.run_callable = run
