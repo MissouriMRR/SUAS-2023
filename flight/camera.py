5c66654c7d1bc8a8363842a51c5bc1d2c1e8f224
Davis <davis@UbuntuDavis.myguest.virtualbox.org>
2023-11-16 19:08:16 -0600

    adding some functions for logic of odlc state

--- flight/camera.py
+++ flight/camera.py
@@ -1,7 +1,10 @@
 """A class that contains all the needed camera functionality for the drone."""
+import json
 import socket
 import logging
+import asyncio
 from xml.etree import ElementTree as ET
+
 
 class Camera:
     def __init__(self) -> None:
@@ -10,13 +13,13 @@ class Camera:
             socket.AF_INET, socket.SOCK_DGRAM
         )
         self.actionURL: str | None = None
-        
+
     async def connect_to_camera(self):
         """Connect to the camera."""
         ssdp_request: str = (
             "M-SEARCH * HTTP/1.1"
             "HOST: 239.255.255.250:1900"
-            "MAN: \"ssdp:discover\""
+            'MAN: "ssdp:discover"'
             "MX: 1"
             "ST: urn:schemas-sony-com:service:ScalarWebAPI:1"
             "USER-AGENT: SUAS-Drone/1.0"
@@ -31,18 +34,111 @@ class Camera:
                 location: str = data.decode().split("LOCATION: ")[1].split("\r\n")[0]
                 logging.info(f"Found actionURL: {self.actionURL}")
                 break
-        
+
         xml_tree = ET.parse(location)
         root = xml_tree.getroot()
-        
+
         for item in root.findall(".//item"):
             name = item.find("name").text
-            if name == "X_ScalarWebAPI_ActionList_URL": # ActionList_URL item name
+            if name == "X_ScalarWebAPI_ActionList_URL":  # ActionList_URL item name
                 value = item.find("value").text
                 logging.info(f"Key: {name}, Value: {value}")
                 break
-        
+
         # Close the socket
         self.ssdp_socket.close()
-        
-        
+
+    async def odlc_move_to(
+        drone: System,
+        latitude: float,
+        longitude: float,
+        altitude: float,
+        fast_param: float,
+        take_photos: float,
+    ) -> None:
+        """
+        This function takes in a latitude, longitude and altitude and autonomously
+        moves the drone to that waypoint. This function will also auto convert the altitude
+        from feet to meters. It will take photos along the path if passed true in take_photos and
+        add the point and name of photo to a json
+
+        Parameters
+        ----------
+        drone: System
+            a drone object that has all offboard data needed for computation
+        latitude: float
+            a float containing the requested latitude to move to
+        longitude: float
+            a float containing the requested longitude to move to
+        altitude: float
+            a float contatining the requested altitude to go to in meters
+        fast_param: float
+            a float that determines if the drone will take less time checking its precise location
+            before moving on to another waypoint. If its 1, it will move at normal speed,
+            if its less than 1(0.83), it will be faster.
+        take_photos:float
+            will take photos with the camera until the position has been reached
+        """
+
+        take_photos = True
+        info: dict[str, dict[str, int | list[int | float] | float]] = {}
+
+        # get current altitude
+        async for terrain_info in drone.telemetry.home():
+            absolute_altitude: float = terrain_info.absolute_altitude_m
+            break
+
+        await drone.action.goto_location(
+            latitude, longitude, altitude + absolute_altitude, 0
+        )
+        location_reached: bool = False
+        # First determine if we need to move fast through waypoints or need to slow down at each one
+        # Then loops until the waypoint is reached
+        while not location_reached:
+            logging.info("Going to waypoint")
+            async for position in drone.telemetry.position():
+                # continuously checks current latitude, longitude and altitude of the drone
+                drone_lat: float = position.latitude_deg
+                drone_long: float = position.longitude_deg
+                drone_alt: float = position.relative_altitude_m
+
+                #  accurately checks if location is reached and stops for 15 secs and then moves on.
+                if (
+                    (
+                        round(drone_lat, int(6 * fast_param))
+                        == round(latitude, int(6 * fast_param))
+                    )
+                    and (
+                        round(drone_long, int(6 * fast_param))
+                        == round(longitude, int(6 * fast_param))
+                    )
+                    and (round(drone_alt, 1) == round(altitude, 1))
+                ):
+                    location_reached = True
+                    logging.info("arrived")
+                    break
+
+            if take_photos:
+                name_tuple: tuple = capture_photo()
+
+                point: dict[str, dict[str, int | list[int | float] | float]] = {
+                    name_tuple[0]: {
+                        "focal_length": 14,
+                        "rotation_deg": [
+                            drone.offboard.Attitude.roll_deg,
+                            drone.offboard.Attitude.pitch_deg,
+                            drone.offboard.Attitude.yaw_deg,
+                        ],
+                        "drone_coordinates": [drone_lat, drone_long],
+                        "altitude_f": drone_alt,
+                    }
+                }
+
+                info.update(point)
+
+                with open("camera.json", "w", encoding="ascii") as camera:
+                    json.dump(info, camera)
+
+            # tell machine to sleep to prevent constant polling, preventing battery drain
+            await asyncio.sleep(1)
+        return
--- state_machine/states/impl/odlc_impl.py
+++ state_machine/states/impl/odlc_impl.py
@@ -1,6 +1,9 @@
 """Implements the behavior of the ODLC state."""
 import asyncio
 import logging
+import json
+
+from flight.camera import Camera
 
 from state_machine.states.airdrop import Airdrop
 from state_machine.states.odlc import ODLC
@@ -26,9 +29,75 @@ async def run(self: ODLC) -> State:
     """
     try:
         logging.info("ODLC")
-        # code for the ODLC logic
 
-        return Airdrop(self.drone)
+        # These waypoint values are all that are needed to traverse the whole odlc drop location
+        # because it is a small rectangle
+        # The first waypoint is the midpoint of
+        # the left side of the rectangle(one of the short sides), the second point is the
+        # midpoint of the right side of the rectangle(other short side),
+        # and the third point is the top left corner of the rectangle
+        # it goes there for knowing where the drone ends to travel to each of the drop locations,
+        # the altitude is locked at 100 because
+        # we want the drone to stay level and the camera to view the whole odlc boundary
+        # the altitude 100 feet was chosen to cover the whole odlc boundary
+        # because the boundary is 70ft by 360ft the fov of the camera
+        # is vertical 52.1 degrees and horizontal 72.5,
+        # so using the minimum length side of the photo the coverage would be 90 feet allowing
+        # 10 feet overlap on both sides
+        waypoint: dict[str, list[float]] = {
+            "lats": [38.31451966813249, 38.31430872867596, 38.31461622313521],
+            "longs": [-76.54519982319357, -76.54397320409971, -76.54516993186949],
+            "Altitude": [100],
+        }
+
+        # traverses the 3 waypoints starting at the midpoint on left to midpoint on the right
+        # then to the top left corner at the rectangle
+        with open("flight/data/output.json", encoding="ascii") as output:
+            airdrop_dict = json.load(output)
+            airdrops: int = len(airdrop_dict)
+            point: int
+        while airdrops != 5:
+            logging.info("Starting odlc zone flyover")
+
+            for point in range(3):
+                take_photos: bool = False
+
+                if point == 0:
+                    logging.info("Moving to the center of the west boundary")
+                elif point == 1:
+                    # starts taking photos at a .5 second interval because we want
+                    # to get multiple photos of the boundary so there is overlap and
+                    # the speed of the drone should be 20 m/s which is 64 feet/s which means
+                    # it will traverse the length of the boundary (360 ft) in 6 sec
+                    # and that means with the shortest length of photos
+                    #  being taken depending on rotation
+                    # would be 90 feet and we want to take multiple photos
+                    # so we would need a minimum of 4 photos to cover
+                    #  the whole boundary and we want multiple,
+                    # so using .5 seconds between each photo allows
+                    # it to take a minimum of 12 photos of
+                    #  the odlc boundary which will capture the whole area
+
+                    logging.info("Moving to the center of the east boundary")
+                    take_photos = True
+
+                elif point == 2:
+                    logging.info("Moving to the north west corner")
+
+                await Camera.odlc_move_to(
+                    drone,
+                    waypoint["lats"][point],
+                    waypoint["longs"][point],
+                    waypoint["Altitude"][0],
+                    5 / 6,
+                    take_photos,
+                )
+
+            with open("flight/data/output.json", encoding="ascii") as output:
+                airdrop_dict = json.load(output)
+                airdrops = len(airdrop_dict)
+
+        return Airdrop(self.drone, self.flight_settings)
     except asyncio.CancelledError as ex:
         logging.error("ODLC state canceled")
         raise ex
