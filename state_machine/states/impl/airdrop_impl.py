6185ba785aa890e44981e8887178e0d63bdf2703
Davis <davis@UbuntuDavis.myguest.virtualbox.org>
2023-11-28 19:12:14 -0600

    adding in last years logic and functions

--- state_machine/states/impl/airdrop_impl.py
+++ state_machine/states/impl/airdrop_impl.py
@@ -26,6 +26,44 @@ async def run(self: Airdrop) -> State:
 
         # code for Airdrop logic
 
+        # setup airdrop
+        airdrop = AirdropControl()
+
+        # For the amount of bottles there are...
+        for num in range(len(bottle_locations)):
+            logging.info("Bottle drop #", num + 1, "started")
+            # Set initial value for lowest distance so we can compare
+            lowest_distance: float = 100000
+            bottle_num: int
+            # Get the drones current position
+            async for position in drone.telemetry.position():
+                current_location: telemetry.Position = position
+                break
+            # For each bottle location, get the distance from the drone
+            for location in bottle_locations.values():
+                distance: float = await self.get_distance(
+                    current_location.latitude_deg,
+                    current_location.longitude_deg,
+                    location["latitude"],
+                    location["longitude"],
+                )
+                if distance < lowest_distance:
+                    lowest_distance = distance
+                    bottle_loc: dict[str, float] = location
+                    bottle_num = [i for i in bottle_locations if bottle_locations[i] == location][0]
+            logging.info(
+                f'Nearest bottle: #{bottle_num} at ({bottle_loc["latitude"]}, {bottle_loc["longitude"]})'
+            )
+            # Move to the nearest bottle
+            await move_to(drone, bottle_loc["latitude"], bottle_loc["longitude"], 80, 1)
+            await airdrop.drop_bottle(bottle_num)
+            await asyncio.sleep(
+                5
+            )  # This will need to be changed based on how long it takes to drop the bottle
+            # Remove the bottle location from the dictionary
+            bottle_locations.pop(bottle_num)
+        logging.info("-- Airdrop done!")
+
         return Waypoint(self.drone, self.flight_settings)
     except asyncio.CancelledError as ex:
         logging.error("Airdrop state canceled")
