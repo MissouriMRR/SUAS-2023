28a24d5393e6a9dcc090fefbcd5e6fad649835f0
s <ouyangyuxuan@ouyangyuxuandeMacBook-Air.local>
2024-04-11 17:56:48 -0500

    change

--- testing1.py
+++ testing1.py
@@ -1,8 +1,11 @@
 import cv2
 import numpy as np
-from vision.standard_object.odlc_classify_shape import classify_shape
+#from vision.standard_object.odlc_classify_shape import classify_shape
 
-filename = "dji_fly_20230930_154916_57_1696107023985_timed.jpg"
+#filename = "dji_fly_20230930_154916_57_1696107023985_timed.jpg"
+
+
+filename = "Copy of dji_fly_20221203_131406_6_1670094877955_photo_optimized.jpg"
 bgr_img = cv2.imread(filename)
 
 # bgr_img = cv2.GaussianBlur(bgr_img, (11,11), 0)
@@ -28,8 +31,8 @@ draw_img = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
 num_detections = 0
 for cnt in contours:
     if len(cnt) > 1:
-        classification = classify_shape(np.copy(cnt), [])
-        if classification is not None:
+        #classification = classify_shape(np.copy(cnt), [])
+        ##if classification is not None:
             # print(classification)
             draw_img = cv2.drawContours(draw_img, [cnt], -1, (0, 255, 0), 5)
             num_detections += 1
